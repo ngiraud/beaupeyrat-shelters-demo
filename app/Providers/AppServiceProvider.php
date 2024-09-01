31395d3841188ef5eba909a13e456bdd2e94273a
Nicolas Giraud <contact@ngiraud.me>
2024-09-01 17:39:06 +0200

    Move user route in a controller

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Dedoc\Scramble\Support\Generator\SecuritySchemes\OAuthFlow;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -21,5 +25,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Passport::enablePasswordGrant();
+
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+            $openApi->secure(
+                SecurityScheme::oauth2()
+                              ->flow('password', function (OAuthFlow $flow) {
+                                  $flow
+                                      ->tokenUrl(route('passport.token'))
+                                      ->refreshUrl(route('passport.token.refresh'));
+                              })
+            );
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::get('/me', [UserController::class, 'me'])
+     ->middleware('auth:api')
+     ->name('me');
