389a47dc27d2575564b976bbc450e765d39dc7b8
Nicolas Giraud <contact@ngiraud.me>
2024-09-15 10:59:20 +0200

    Rename animal migration to be after species

--- database/migrations/2025_09_01_154033_create_animals_table.php
+++ database/migrations/2025_09_01_154033_create_animals_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,9 +15,9 @@ return new class extends Migration
             $table->id();
 
             $table->foreignIdFor(Shelter::class)
-                ->constrained()
-                ->cascadeOnDelete()
-                ->cascadeOnUpdate();
+                  ->constrained()
+                  ->cascadeOnDelete()
+                  ->cascadeOnUpdate();
 
             $table->string('name');
             $table->text('description');
