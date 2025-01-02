f7c5aa7287f4b571e8ac5f64d8169fb683e549bc
Nicolas Giraud <contact@ngiraud.me>
2025-01-02 17:59:34 +0100

    fix: phpstan

--- app/Models/Shelter.php
+++ app/Models/Shelter.php
@@ -10,13 +10,13 @@ class Shelter extends Model
 {
     use HasFactory;
 
-    /** @return HasMany<User> */
+    /** @return HasMany<User, covariant $this> */
     public function users(): HasMany
     {
         return $this->hasMany(User::class);
     }
 
-    /** @return HasMany<Animal> */
+    /** @return HasMany<Animal, covariant $this> */
     public function animals(): HasMany
     {
         return $this->hasMany(Animal::class);
