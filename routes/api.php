4263b02e1cb7b37c8dfe2f75679826b1aa3c0ee5
Nicolas Giraud <contact@ngiraud.me>
2025-01-15 16:35:24 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,12 @@ use App\Http\Controllers\SpeciesController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/me', [UserController::class, 'me'])
-    ->middleware('auth:api')
-    ->name('me');
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', [UserController::class, 'me'])->name('me');
 
-Route::apiResource('animal', AnimalController::class)
-    ->middleware('auth:api');
+    Route::apiResource('animal', AnimalController::class);
 
-Route::apiResource('shelter', ShelterController::class)
-    ->middleware('auth:api');
+    Route::apiResource('shelter', ShelterController::class);
 
-Route::apiResource('species', SpeciesController::class)
-    ->middleware('auth:api');
+    Route::apiResource('species', SpeciesController::class);
+});
